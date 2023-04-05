3674fce5c54d2bfd8e9c8fe205cbbfc0f69fc58c
Jordan Roland <jdnal7@gmail.com>
2023-04-05 14:44:17 -0500

    flex box ironed out. finalizing layout

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,19 @@ return (
     <Navbar />
     <div className="flex flex-wrap gap-2 h-full">
         
-        <div className=" text-white h-26 flex-auto ml-20 mr-10 mt-5">
-            <img src={shadowPic} alt="picture of me" />
+        <div className=" text-white max-w-1/2 h-30 w-26 flex-auto ml-20 mr-10 mt-5">
+            <img className="flex-auto h-60 w-45 ml-8" src={shadowPic} alt="picture of me" />
         </div>
-        <div className="bg-blue-500 text-white h-26 max-w-1/2 flex-auto mt-5">
-            <div className="char_bio">
-                
+        <div className=" text-white h-26 max-w-1/2 flex-auto mt-5">
+            <div className="">
+                <h2 className="my_name mt-20 text-4xl font-['Roboto']">
+                    Jordan Roland
+                </h2>
+                <p className="italic">Full Stack Engineer</p>
+                <br></br>
+                <p className="char_bio">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
             </div>
         </div>
     </div>
