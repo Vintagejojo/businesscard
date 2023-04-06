449d40c8b30b371e4c8d7da69375c024361ce1f0
Jordan Roland <jdnal7@gmail.com>
2023-04-06 09:39:41 -0500

    small changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,7 @@ import {BsLinkedin, BsTwitter, BsFacebook } from 'react-icons/bs';
 
 function Navbar() {
     return (
-        <div className=" left-0 z-0 h-screen w-16 flex flex-col gap-x-10 bg-primary text-stone-400">
-            <br className='superspace'/>
+        <div className="fixed left-0 z-0 h-screen w-16 flex flex-col gap-x-10 bg-primary text-stone-400">
             <NavbarIcon icon={<BsLinkedin size="28" />} />
             <NavbarIcon icon={<BsTwitter size="28" />} />
             <NavbarIcon icon={<BsFacebook size="28" />} />
